6fac08d77aafcbed82771214ae06795d3591f1f8
Kamil <55342474+Kamillion1@users.noreply.github.com>
2019-12-02 17:27:33 -0600

    The other games were implemented well

--- app/src/main/java/com/example/finalproject/SlotsActivity.java
+++ app/src/main/java/com/example/finalproject/SlotsActivity.java
@@ -1,10 +1,11 @@
 package com.example.finalproject;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+
 public class SlotsActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
