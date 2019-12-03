1cae948e60ece3fd0c42067426ee1eec26cf5d26
David Matthews <davidwm2@illinois.edu>
2019-12-02 18:18:47 -0600

    blackjack layout

--- app/src/main/java/com/example/finalproject/BlackjackActivity.java
+++ app/src/main/java/com/example/finalproject/BlackjackActivity.java
@@ -6,18 +6,16 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class BlackjackActivity extends AppCompatActivity {
-    Deck deck = new Deck(); // initializes the deck we use to play.
-    Hand playerHand = new Hand(deck);
+    Deck deck; // initializes the deck we use to play.
+    Hand playerHand;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blackjack);
-                TextView textView = (TextView) findViewById(R.id.myFirst);
-                textView.setText(playerHand.hand.get(0).getValue());
 
-                textView = (TextView) findViewById(R.id.mySecond);
-                textView.setText(playerHand.hand.get(1).getValue());
+        Deck deck = new Deck();
+        playerHand = new Hand(deck);
     }
 
 }
