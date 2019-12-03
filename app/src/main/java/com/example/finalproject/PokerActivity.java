2669bf27716b5be7ce2945a999c371b20960e262
Kamil <55342474+Kamillion1@users.noreply.github.com>
2019-12-02 18:01:52 -0600

    Poker is set up

--- app/src/main/java/com/example/finalproject/PokerActivity.java
+++ app/src/main/java/com/example/finalproject/PokerActivity.java
@@ -3,6 +3,9 @@ package com.example.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 
 public class PokerActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class PokerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_poker);
+        // Calling all the objects in the 5 hand poker game.
+        Button deal = findViewById(R.id.dealButton);
+        CheckBox cardOne = findViewById(R.id.cardOne);
+        CheckBox cardTwo = findViewById(R.id.cardTwo);
+        CheckBox cardThree = findViewById(R.id.cardThree);
+        CheckBox cardFour = findViewById(R.id.cardFour);
+        CheckBox cardFive = findViewById(R.id.cardFive);
+        deal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private static void main() {
+
     }
 }
