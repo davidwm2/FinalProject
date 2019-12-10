6091bde5ca737cff13e2a8e0c12119ba78266b25
Kamil <55342474+Kamillion1@users.noreply.github.com>
2019-12-10 14:36:12 -0600

    implementing a dealer.

--- app/src/main/java/com/example/finalproject/PokerActivity.java
+++ app/src/main/java/com/example/finalproject/PokerActivity.java
@@ -120,6 +120,7 @@ public class PokerActivity extends AppCompatActivity {
         finThree.setText(playerHand.hand.get(2).toString());
         finFour.setText(playerHand.hand.get(3).toString());
         finFive.setText(playerHand.hand.get(4).toString());
+        dealerMoves(deck);
         home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -171,5 +172,8 @@ public class PokerActivity extends AppCompatActivity {
             return true;
         }
         return false;
+    }
+    private void dealerMoves(Deck deck) {
+
     }
 }
