47b8c172c7bc6b0bda93f0eb228be45433e243b1
Kamil <55342474+Kamillion1@users.noreply.github.com>
2019-12-10 14:26:41 -0600

    Got from counting pairs to full house. Need to implement dealer and maybe more ways to win

--- app/src/main/java/com/example/finalproject/PokerActivity.java
+++ app/src/main/java/com/example/finalproject/PokerActivity.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class PokerActivity extends AppCompatActivity {
     /**
      * to check which card are set to be pulled.
@@ -41,6 +43,7 @@ public class PokerActivity extends AppCompatActivity {
         final CheckBox cardThree = findViewById(R.id.cardThree);
         final CheckBox cardFour = findViewById(R.id.cardFour);
         final CheckBox cardFive = findViewById(R.id.cardFive);
+        pairs = 0;
         //hand setup
         deck = new Deck();
         deck.shuffle();
@@ -77,30 +80,46 @@ public class PokerActivity extends AppCompatActivity {
     private void dealt() {
         Button home = findViewById(R.id.homeButton);
         TextView endText = findViewById(R.id.endText);
+        TextView finOne = findViewById(R.id.cardOneFinal);
+        TextView finTwo = findViewById(R.id.cardTwoFinal);
+        TextView finThree = findViewById(R.id.cardThreeFinal);
+        TextView finFour = findViewById(R.id.cardFourFinal);
+        TextView finFive = findViewById(R.id.cardFiveFinal);
         boolean firstCheck = true;
         String message = "You threw in";
         int j = 1;
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 4; i++) {
             if (check[i] && firstCheck) {
+                System.out.println("i ran");
                 message = message + " " + (playerHand.hand.get(i).toString());
+                playerHand.hand.set(i, deck.drawCard());
                 firstCheck = false;
             } else if (check[i]) {
+                System.out.println("i ran too");
                 message = message + ", " + (playerHand.hand.get(i).toString());
+                playerHand.hand.set(i, deck.drawCard());
             }else {
                 j++;
             }
         }
-        if (j > 5) {
-            endText.setText("You held your cards.");
+        pairs = pairFinder(playerHand.hand);
+        /*if (j > 4) {
+            endText.setText("You held your cards.  You have " + pairs +" pairs.");
+        } */
+        if (fullHouse(playerHand.hand)){
+            endText.setText(message + ". You have a full house.");
+        }else if (threeOfAKind(playerHand.hand)) {
+            endText.setText(message + ". You have three of a kind.");
+        } else if (pairs == 1){
+            endText.setText(message + ". You have " + pairs +" pair.");
         } else {
-            endText.setText(message + ".");
+            endText.setText(message + ". You have " + pairs +" pairs.");
         }
-
-        for (int i = 0; i < 4; i++) {
-            for (int k = i + 1; k <= 4 - i; k++) {
-                pairFinder(i, k);
-            }
-        }
+        finOne.setText(playerHand.hand.get(0).toString());
+        finTwo.setText(playerHand.hand.get(1).toString());
+        finThree.setText(playerHand.hand.get(2).toString());
+        finFour.setText(playerHand.hand.get(3).toString());
+        finFive.setText(playerHand.hand.get(4).toString());
         home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -108,7 +127,49 @@ public class PokerActivity extends AppCompatActivity {
             }
         });
     }
-    private int pairFinder(int current, int other) {
-        if (playerHand.hand.get(current).getSuit().)
+
+    /**
+     * used to find pair
+     * @return
+     */
+    private int pairFinder(ArrayList<Card> cards) {
+        int pears = 0;
+        boolean firstPair;
+        for (int i = 0; i < 4; i++) {
+            firstPair = true;
+            for (int k = i + 1; k <= 4; k++) {
+                if (cards.get(i).getRank() == cards.get(k).getRank() && firstPair) {
+                    pears++;
+                    firstPair = false;                }
+            }
+        }
+        return pears;
+    }
+
+    /**
+     * Checks for 3 of a kind
+     * @param cards is the player's cards
+     * @return if three of a kind
+     */
+    private boolean threeOfAKind(ArrayList<Card> cards) {
+        int triple = 0;
+        for (int i = 0; i < 2; i++) {
+            triple = 0;
+            for (int j = i + 1; j < 4; j++) {
+                if (cards.get(i).getRank() == cards.get(j).getRank()) {
+                    triple++;
+                    if (triple == 2) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    private boolean fullHouse(ArrayList<Card> cards) {
+        if (threeOfAKind(cards) && pairFinder(cards) == 1) {
+            return true;
+        }
+        return false;
     }
 }
